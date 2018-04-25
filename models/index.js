bf02e51be1f67bc421db51d01eab249ee09d80d0
ElijahHHall <ehall851@gmail.com>
2018-04-25 15:54:58 -0600

    Heroku gods hear me

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/KingWear');
+mongoose.connect('mongodb://localhost/kingwear2');
 
 if (process.env.NODE_ENV == 'production') {
 	mongoose.connect(process.env.MLAB_URL);
 } else {
-	mongoose.connect('mongodb://localhost/KingWear');
+	mongoose.connect('mongodb://localhost/kingwear2');
 }
 
 module.exports.Shoe = require('./shoes.js');
