5aa9b4b32455e2068786840dbde75dadb858fd7c
ElijahHHall <ehall851@gmail.com>
2018-04-26 16:04:31 -0600

    uh oh

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$('#signup-form').on('submit', function(e){
+		e.preventDefault();
+
+		var formInfo = {
+			email: $('#email-input').val(),
+			password: $('#pass-input').val()
+		};
+
+		console.log(formInfo);
+
+		$.ajax({
+			url: '/signup',
+			method: 'POST',
+			data: formInfo,
+			succes: function(response){
+				$('h1').append(response.email + 'is now signed up!');
+			}
+		});
+	});
+
+	$('#login-form').on('submit', function(e){
+		e.preventDefault();
+
+		var formInfo = {
+			email: $('#email-input').val(),
+			password: $('#pass-input').val()
+		};
+		console.log(formInfo);
+
+		$.ajax({
+			url: '/sessions',
+			method: 'POST',
+			data: formInfo,
+			succes: function(response){
+				window.location = '/profile';
+			}
+		});
+	});
+});
