fd4aad0081ede255de1ed4928f241a0a70412811
ElijahHHall <ehall851@gmail.com>
2018-04-27 15:18:18 -0600

    catch up to current branch

---
+++ public/scripts/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	$('#signup-form').on('submit', function(e){
+		e.preventDefault();
+
+		var formInfo = {
+			email: $('#email-input').val(),
+			password: $('#pass-input').val()
+		};
+
+		console.log(formInfo);
+
+		$.ajax({
+			url: '/signup',
+			method: 'POST',
+			data: formInfo,
+			succes: function(response){
+				$('h1').append(response.email + 'is now signed up!');
+			}
+		});
+	});
+
+	$('#login-form').on('submit', function(e){
+		e.preventDefault();
+
+		var formInfo = {
+			email: $('#email-input').val(),
+			password: $('#pass-input').val()
+		};
+		console.log(formInfo);
+
+		$.ajax({
+			url: '/login',
+			method: 'POST',
+			data: formInfo,
+			succes: function(response){
+				window.location = '/profile';
+			}
+		});
+	});
+});
+		console.log('submit');
