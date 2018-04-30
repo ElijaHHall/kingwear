c6cd89769fb109cbed8a2bc7d8dd4738e4e2cf39
ElijahHHall <ehall851@gmail.com>
2018-04-30 10:38:15 -0600

    changed from kingwear 2 to just kingwear

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 if (process.env.NODE_ENV == 'production') {
 	mongoose.connect(process.env.MLAB_URL);
 } else {
-	mongoose.connect('mongodb://localhost/kingwear2');
+	mongoose.connect('mongodb://localhost/kingwear');
 }
 
 module.exports.Shoe = require('./shoes.js');
